4ddf4f862d8e340a7c7722df7283e19756e5ede4
wesleyy18 <wesleyyvargas@gmail.com>
2021-11-13 12:01:57 -0400

    primeiro commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    $tabuleiro["A"][1]="Agua";
+    $tabuleiro["A"][2]="Agua";
+    $tabuleiro["A"][3]="Agua";
+    $tabuleiro["A"][4]="Agua";
+    $tabuleiro["A"][5]="Agua";
+    
+    $tabuleiro["B"][1]="Agua";
+    $tabuleiro["B"][2]="Agua";
+    $tabuleiro["B"][3]="Agua";
+    $tabuleiro["B"][4]="Agua";
+    $tabuleiro["B"][5]="Agua";
+
+    $tabuleiro["C"][1]="Agua";
+    $tabuleiro["C"][2]="Agua";
+    $tabuleiro["C"][3]="Agua";
+    $tabuleiro["C"][4]="Agua";
+    $tabuleiro["C"][5]="Agua";
+
+    $tabuleiro["D"][1]="Agua";
+    $tabuleiro["D"][2]="Agua";
+    $tabuleiro["D"][3]="Agua";
+    $tabuleiro["D"][4]="Agua";
+    $tabuleiro["D"][5]="Agua";
+
+    $tabuleiro["E"][1]="Agua";
+    $tabuleiro["E"][2]="Agua";
+    $tabuleiro["E"][3]="Agua";
+    $tabuleiro["E"][4]="Agua";
+    $tabuleiro["E"][5]="Agua";
+
+
+
+
+?>          
